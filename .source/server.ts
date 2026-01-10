b1d2c1e1796e8e85395278eefa4f24dd0cb4bf7d
pantaleone-ai <mattpantaleone@gmail.com>
2026-01-10 08:25:41 -0700

    fix some img references

--- .source/server.ts
+++ .source/server.ts
@@ -26,11 +26,11 @@ import * as __fd_glob_31 from "../features/projects/content/mixphd.mdx?collectio
 import * as __fd_glob_30 from "../features/projects/content/migratecms-migrate-anycms-to-nextjs.mdx?collection=projects"
 import * as __fd_glob_29 from "../features/projects/content/imgsquash.mdx?collection=projects"
 import * as __fd_glob_28 from "../features/projects/content/ai-saas-starter-nextjs-betterauth-supabase-free-boilerplate.mdx?collection=projects"
-import * as __fd_glob_27 from "../features/privacy/content/terms.mdx?collection=privacy"
-import * as __fd_glob_26 from "../features/privacy/content/privacy.mdx?collection=privacy"
-import * as __fd_glob_25 from "../features/home/content/featured-apps/qr-code-generator.mdx?collection=featuredApps"
-import * as __fd_glob_24 from "../features/home/content/featured-apps/profitsignals-ai-agent.mdx?collection=featuredApps"
-import * as __fd_glob_23 from "../features/home/content/featured-apps/nextjs-saas-starter.mdx?collection=featuredApps"
+import * as __fd_glob_27 from "../features/home/content/featured-apps/qr-code-generator.mdx?collection=featuredApps"
+import * as __fd_glob_26 from "../features/home/content/featured-apps/profitsignals-ai-agent.mdx?collection=featuredApps"
+import * as __fd_glob_25 from "../features/home/content/featured-apps/nextjs-saas-starter.mdx?collection=featuredApps"
+import * as __fd_glob_24 from "../features/privacy/content/terms.mdx?collection=privacy"
+import * as __fd_glob_23 from "../features/privacy/content/privacy.mdx?collection=privacy"
 import * as __fd_glob_22 from "../features/experience/content/experience.mdx?collection=experience"
 import * as __fd_glob_21 from "../features/education/content/education.mdx?collection=education"
 import * as __fd_glob_20 from "../features/changelog/content/changelog.mdx?collection=changelog"
@@ -145,9 +145,9 @@ export const education = await create.docs("education", "features/education/cont
 
 export const experience = await create.docs("experience", "features/experience/content", {}, {"experience.mdx": __fd_glob_22, });
 
-export const featuredApps = await create.docs("featuredApps", "features/home/content/featured-apps", {}, {"nextjs-saas-starter.mdx": __fd_glob_23, "profitsignals-ai-agent.mdx": __fd_glob_24, "qr-code-generator.mdx": __fd_glob_25, });
+export const featuredApps = await create.docs("featuredApps", "features/home/content/featured-apps", {}, {"nextjs-saas-starter.mdx": __fd_glob_25, "profitsignals-ai-agent.mdx": __fd_glob_26, "qr-code-generator.mdx": __fd_glob_27, });
 
-export const privacy = await create.docs("privacy", "features/privacy/content", {}, {"privacy.mdx": __fd_glob_26, "terms.mdx": __fd_glob_27, });
+export const privacy = await create.docs("privacy", "features/privacy/content", {}, {"privacy.mdx": __fd_glob_23, "terms.mdx": __fd_glob_24, });
 
 export const projects = await create.docs("projects", "features/projects/content", {}, {"ai-saas-starter-nextjs-betterauth-supabase-free-boilerplate.mdx": __fd_glob_28, "imgsquash.mdx": __fd_glob_29, "migratecms-migrate-anycms-to-nextjs.mdx": __fd_glob_30, "mixphd.mdx": __fd_glob_31, "pantaleonenet.mdx": __fd_glob_32, "profitsignals-ai-chat-agent.mdx": __fd_glob_33, "qrcode-generator-free-nextjs15-tailwind.mdx": __fd_glob_34, "skillsnap-increase-ai-skills.mdx": __fd_glob_35, });
 
