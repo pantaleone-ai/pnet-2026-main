775c71705ad97b2745641b6f254b21202087c2ee
pantaleone-ai <mdptrading@gmail.com>
2026-09-21 13:29:36 -0600

    Remove Vercel Speed Insights

--- components/Providers.tsx
+++ components/Providers.tsx
@@ -3,7 +3,6 @@
 import { AppProgressProvider } from "@bprogress/next";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Analytics } from "@vercel/analytics/next";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 import { RootProvider } from "fumadocs-ui/provider/next";
 import { Provider as JotaiProvider } from "jotai";
 import { ThemeProvider } from "next-themes";
@@ -39,7 +38,6 @@ export function Providers({ children }: { children: React.ReactNode }) {
 
             <Toaster />
             <Analytics />
-            <SpeedInsights />
           </ThemeProvider>
         </QueryClientProvider>
       </JotaiProvider>
